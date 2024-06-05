19a50728bce752994d83665ea5b1857ba658875e
Josue Baez <josuejbaeze4gmail.com>
2024-06-05 19:03:14 -0400

    install cloudinary

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import {v2 as cloudinary} from 'cloudinary';
+
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_NAME, 
-    api_key: process.env.CLOUDINARY_APY_KEY, 
-    api_secret: process.env.CLOUDINARY_APY_SECRET
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET,
 });
 
 export async function POST(request){
