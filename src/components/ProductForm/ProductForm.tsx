11fa411ea222c96b6adb19dcbabae2b9d2ddbaac
Josue Baez <josuejbaeze4gmail.com>
2024-08-22 19:06:32 -0400

    New proposal with React hook form, Zod, Typescript

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -4,12 +4,17 @@
 import { useEffect } from 'react';
 import { useFormState } from 'react-dom';
 import { AddProduct, UpdateProduct } from 'src/actions';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import { ProductSchema } from 'src/utils/validation.zod';
 
-const initialState = {
-  errors: [],
-  shouldClose: false,
-  newProduct: null
-};
+type Inputs = {
+  name: string;
+  description: string;
+  category: string;
+  price: string;
+  quantity: string;
+}
 
 type FormProductProps = {
   onClose: () => void;
@@ -21,48 +26,26 @@ type FormProductProps = {
 export const FormProduct = ({
   onClose,
   onAddProduct,
-  onUpdateProduct,
-  product,
+  // onUpdateProduct,
+  // product,
 }: FormProductProps) => {
-  
-  const [status, formAction] = useFormState(
-    product ? UpdateProduct : AddProduct, 
-    initialState
-  );
 
-  useEffect(() => {
-    if (status.shouldClose) {
-      if (product) {
-        onUpdateProduct(status.newProduct as FormData);
-      } else {
-        onAddProduct(status.newProduct as FormData);
-      }
-      onClose();
+  const { register, handleSubmit ,formState: {errors} } = useForm<Inputs>({
+    resolver: zodResolver(ProductSchema),
+  }); 
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error('Error al guardar el producto:', error);
     }
-  }, [status.shouldClose, status.newProduct,  onClose, onAddProduct, onUpdateProduct, product]);
+  };
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-md">
-      <form action={formAction} className="w-full max-w-lg mx-auto bg-gray-900 p-8 rounded-md shadow-md border-2 border-x-gray-600 ">
-        <h2 className="text-2xl text-center font-bold mb-6">{product ? 'Editar Producto' : 'Nuevo Producto'}</h2>
-        
-        {product ? 
-        (
-          <div className="mb-4">
-            <label className="flex items-center justify-center text-sm font-bold mb-2 gap-2" htmlFor="id">
-              ID:
-              <input
-              type="text"
-              id="id"
-              name="id"
-              defaultValue={product?._id || ''}
-              className="border-none w-full py-2 px-3 bg-gray-900"
-              required
-              readOnly
-              />
-            </label>
-          </div>
-        ) : null }
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-lg mx-auto bg-gray-900 p-8 rounded-md shadow-md border-2 border-x-gray-600 ">
+        <h2 className="text-2xl text-center font-bold mb-6">Nuevo Producto</h2>
         
         <div className="mb-4">
           <label className="block text-sm font-bold mb-2" htmlFor="name">
@@ -71,13 +54,12 @@ export const FormProduct = ({
           <input
             type="text"
             id="name"
-            name="name"
-            defaultValue={product?.name || ''}
+            {...register('name')}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
           />
         </div>
-        
+        {errors.name?.message && <p className='text-red-700 pb-2'>{errors.name.message}</p>}
+
         <div className="mb-4">
           <label className="block text-sm font-bold mb-2" htmlFor="description">
             Descripción
@@ -85,11 +67,11 @@ export const FormProduct = ({
           <input
             type="text"
             id="description"
-            name="description"
-            defaultValue={product?.description || ''}
+            {...register('description')}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
+        {errors.description?.message && <p className='text-red-700 pb-2'>{errors.description.message}</p>}
         
         <div className="mb-4">
           <label className="block text-sm font-bold mb-2" htmlFor="category">
@@ -98,27 +80,25 @@ export const FormProduct = ({
           <input
             type="text"
             id="category"
-            name="category"
-            defaultValue={product?.category || ''}
+            {...register('category')}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
           />
         </div>
+        {errors.category?.message && <p className='text-red-700 pb-2'>{errors.category.message}</p>}
         
         <div className="mb-4">
           <label className="block text-sm font-bold mb-2" htmlFor="quantity">
             Cantidad
           </label>
           <input
-            type="number"
+            
             id="quantity"
-            name="quantity"
-            defaultValue={product?.quantity || ''}
+            {...register('quantity')}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
           />
         </div>
-        
+        {errors.quantity?.message && <p className='text-red-700 pb-2'>{errors.quantity.message}</p>}
+
         <div className="mb-4">
           <label className="block text-sm font-bold mb-2" htmlFor="price">
             Precio
@@ -126,21 +106,20 @@ export const FormProduct = ({
           <input
             type="float"
             id="price"
-            name="price"
-            defaultValue={product?.price || ''}
+            {...register('price')}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
           />
         </div>
+        {errors.price?.message && <p className='text-red-700 pb-2'>{errors.price.message}</p>}
         
         <div className="flex items-center justify-between">
           <button
             type="submit"
             className="bg-emerald-600 hover:bg-emerald-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
-            {product ? 'Actualizar' : 'Agregar'}
+            Agregar
           </button>
-          
+      
           <button
             type="button"
             onClick={onClose}
@@ -149,9 +128,8 @@ export const FormProduct = ({
             Cancelar
           </button>
         </div>
-        {status?.errors ? status.errors.map((error: any, index: number) => <p className="text-sm text-center" key={`${error.message}-${index}`}>{error.message}</p>) : null}
-        {status?.error ? <p className="text-sm text-center">{status.error}</p> : null}
       </form>
+
     </div>
   );
 };
--- src/utils/validation.zod.ts
+++ src/utils/validation.zod.ts
@@ -1,5 +1,13 @@
 import { z } from "zod"
 
+const roles = ["administrador", "lider"] as const;
+export type Roles = (typeof roles)[number];
+
+export const mapRoles: {[key in Roles]: string} = {
+    administrador: "Administrador",
+    lider: "Líder"
+}
+
 export const loginSchema = z.object({
     email: z.string().email({ message: "Correo electrónico inválido" }),
     password: z.string().min(8, { message: "Las contraseñas son mayor o igual a 8 caracteres" })
@@ -9,15 +17,15 @@ export const SignupSchema = z.object({
     username: z.string().min(5, { message: "EL nombre de usuario debe ser mayor a 5 caracteres" }),
     email: z.string().email({ message: "Correo electrónico inválido" }),
     password: z.string().min(8, { message: "Las contraseñas son mayor o igual a 8 caracteres" }),
-    role: z.enum(["administrador", "lider"]),
-    secret_question: z.string(),
-    secret_answer: z.string(),
+    role: z.enum(["administrador", "lider"], { message: "Seleccione un rol"}),
+    secret_question: z.string({ message: "Por favor ingrese una pregunta secreta"}),
+    secret_answer: z.string({ message: "Por favor ingrese una respuesta a la pregunta"}),
 })
 
 export const ProductSchema = z.object({
     name: z.string().min(3, { message: "El nombre debe ser mayor o igual a 3 caracteres" }).max(25, { message: "El nombre debe ser menor o igual a 25 caracteres" }),
     description: z.string().max(25, { message: "La descripción debe ser menor a 25 caracteres" }).nullish(),
-    category: z.string().max(25, { message: "La categoría debe ser menor a 25 caracteres" }),
-    price: z.number().min(0).lte(99999),
-    quantity: z.number().min(0).int().lte(10000),
+    category: z.string().min(3, { message: "La categoría debe ser mayor a 3 caracteres" }).max(25, { message: "La categoría debe ser menor a 25 caracteres" }),
+    price: z.string().refine(price => !isNaN(parseFloat(price)), { message: "el precio no es un número"}),
+    quantity: z.string().refine(quantity => /^[0-9]+$/.test(quantity), { message: "el precio no es un número entero" }),
 })
